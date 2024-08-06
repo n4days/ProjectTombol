d74d5326f624b97fe4e74fd438ba44cd4e67db55
n4days <alirsyhad.ramadhan49@gmail.com>
2024-08-06 15:57:18 +0700

    Menambahkan menu dan 3 activity

--- app/src/main/java/com/example/dtshello/MainActivity.java
+++ app/src/main/java/com/example/dtshello/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.dtshello;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -28,5 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.menu1){
+            startActivity(new Intent(this, Menu1.class));
+        } else if (item.getItemId()==R.id.menu2) {
+            startActivity(new Intent(this, Menu2.class));
+        } else if (item.getItemId()==R.id.menu3) {
+            startActivity(new Intent(this, Menu3.class));
+        }
+
+        return true;
     }
 }
